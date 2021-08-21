5f27573ecac50198fbf7dbbe239841c6c0101504
vjspranav <pranavasri@live.in>
2021-08-22 02:34:58 +0530

    Update Configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,18 +3,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'My Research Records',
+  tagline: 'If we knew what it was we were doing, it would not be called research, would it? ~ Albert Einstein',
+  url: 'https://vjspranav.github.io/',
+  baseUrl: '/ResearchRecords/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'vjspranav', // Usually your GitHub org/user name.
+  projectName: 'ResearchRecords', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Research Records',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -87,13 +87,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/vjspranav/ResearchRecords/edit/documentation/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/vjspranav/ResearchRecords/edit/documentation/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
